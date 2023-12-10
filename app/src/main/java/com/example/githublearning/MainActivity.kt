54ad402f2cd636759f0511c1150b41609b6b2107
Kunal Gaba <Kunalgaba44@gmail.com>
2023-12-10 15:44:47 -0500

    Added comment

--- app/src/main/java/com/example/githublearning/MainActivity.kt
+++ app/src/main/java/com/example/githublearning/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    // Added a comment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
